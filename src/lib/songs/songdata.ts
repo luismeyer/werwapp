68f34238f26d39e43851092c6cf93bb11f544c26
Bjarne Rentz <Bjarnerentz@live.de>
2023-01-04 20:51:19 +0100

    feat: Added own endpoint to fetch songs

---
+++ src/lib/songs/songdata.ts
@@ -0,0 +1,21 @@
+import type { Songs } from './song';
+
+export const songData: Songs = {
+	daySongs: [
+		{
+			artist: 'Kevin MacLeod',
+			title: 'Cattails',
+			internalUrl:
+				'https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Kevin_MacLeod/Contemporary_Sampler/Kevin_MacLeod_-_Cattails.mp3?download=1&name=Kevin%20MacLeod%20-%20Cattails.mp3'
+		}
+	],
+
+	nightSongs: [
+		{
+			artist: 'Serat',
+			title: 'Dark decision',
+			internalUrl:
+				'https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/bi02MvmewbLAhlCPHFZYSNNwIoxsbv4VbojwGuUE.mp3?download=1&name=Serat%20-%20Dark%20decision.mp3'
+		}
+	]
+};
