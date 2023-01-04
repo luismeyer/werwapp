94859ca6dd6f4601e73463a6931f120703c230df
Chiara Tidau <47692003+ChiaraTidau@users.noreply.github.com>
2023-01-04 21:34:40 +0000

    fix: handle music with tab change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import Game from '../components/game.svelte';
 	import Settings from '../components/settings.svelte';
 	import '../app.css';
+	import { CrossFade, Player } from 'tone';
 
 	// NOTE: the element that is using one of the theme attributes must be in the DOM on mount
 	onMount(() => {
@@ -14,6 +15,26 @@
 
 	let tabs = ['game', 'settings'];
 	let activeTab = 0;
+
+	let crossFade: CrossFade;
+	let dayTone: Player;
+	let nightTone: Player;
+
+	const startGame = async () => {
+		crossFade = new CrossFade().toDestination();
+		dayTone = new Player();
+		nightTone = new Player();
+
+		nightTone.loop = true;
+
+		// bind day to 0
+		dayTone.connect(crossFade.a);
+
+		// bind night to 1
+		nightTone.connect(crossFade.b);
+
+		crossFade.fade.value = 1;
+	};
 </script>
 
 <header>
@@ -21,14 +42,17 @@
 		<h1 class="text-5xl font-bold">{$t('game.name')}</h1>
 	</div>
 </header>
-<main>
-	<div class="px-8 pt-8">
-		{#if activeTab === 0}
-			<Game />
-		{:else if activeTab === 1}
-			<Settings />
+
+<main class="px-8 pt-8">
+	{#if activeTab === 0}
+		{#if !crossFade || !dayTone || !nightTone}
+			<button on:click={startGame} class="btn btn-primary">Start Game</button>
+		{:else}
+			<Game {crossFade} {dayTone} {nightTone} />
 		{/if}
-	</div>
+	{:else if activeTab === 1}
+		<Settings />
+	{/if}
 </main>
 
 <div class="btm-nav">
