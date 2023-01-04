ff959619e361aec91692ad4c560e8fa10f61aec3
FredmFred <frederick.theiss@t-online.de>
2023-01-04 17:02:31 +0000

    feat: Add Settings page

--- src/lib/translation/translations.js
+++ src/lib/translation/translations.js
@@ -1,29 +1,39 @@
 export default {
-    en: {
-      "role.hunter.name": "hunter",
-      "role.villager.name": "villager",
-      "role.armor.name": "armor",
-      "role.visionary.name": "visionary",
-      "role.werwolf.name": "werwolf",
-      "role.vilagehoe.name": "vilagehoe",
-      "role.witch.name": "witch",
-      "gamestate.night": "Night",
-      "gamestate.day": "Day",
-      "game.game": "Game",
-      "game.settings": "Settings"
-    },
+	en: {
+		'role.hunter.name': 'hunter',
+		'role.villager.name': 'villager',
+		'role.armor.name': 'armor',
+		'role.visionary.name': 'visionary',
+		'role.werwolf.name': 'werwolf',
+		'role.vilagehoe.name': 'vilagehoe',
+		'role.witch.name': 'witch',
+		'gamestate.night': 'Night',
+		'gamestate.day': 'Day',
+		game: 'Game',
+		settings: 'Settings',
+		en: 'english',
+		de: 'german',
+		'game.name': 'Werwolf',
+		'settings.discribtion': 'Here you can edit your game settings.',
+		'settings.toggel_themes': 'Automatic collor scheme'
+	},
 
-    de: {
-      "role.hunter.name": "Jäger",
-      "role.villager.name": "Dorfbewohner",
-      "role.armor.name": "Amor",
-      "role.visionary.name": "Seherin",
-      "role.werwolf.name": "Werwolf",
-      "role.vilagehoe.name": "Dorfmatratze",
-      "role.witch.name": "Hexe",
-      "gamestate.night": "Nacht",
-      "gamestate.day": "Tag",
-      "game.game": "Spiel",
-      "game.settings": "Einstellungen"
-    },
-  };
+	de: {
+		'role.hunter.name': 'Jäger',
+		'role.villager.name': 'Dorfbewohner',
+		'role.armor.name': 'Amor',
+		'role.visionary.name': 'Seherin',
+		'role.werwolf.name': 'Werwolf',
+		'role.vilagehoe.name': 'Dorfmatratze',
+		'role.witch.name': 'Hexe',
+		'gamestate.night': 'Nacht',
+		'gamestate.day': 'Tag',
+		game: 'Spiel',
+		settings: 'Einstellungen',
+		'settings.discribtion': 'Hier kannst du deine Spieleinstellungen bearbeiten.',
+		'settings.toggel_themes': 'Dynamischer Farbschemawechsel',
+		en: 'Englisch',
+		de: 'Deutsch',
+		'game.name': 'Werwolf'
+	}
+};
