1630f0c80364a8491c8e184e992f5787133889f2
Bjarne Rentz <Bjarnerentz@live.de>
2023-01-09 20:33:48 +0100

    feat: Server-Side song caching

--- src/routes/api/songs/+server.ts
+++ src/routes/api/songs/+server.ts
@@ -1,10 +1,34 @@
 import type { RequestEvent } from './$types';
+import fs from 'fs';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ url }: RequestEvent) {
 	const songUrl = url.searchParams.get('url') ?? '';
+	const internalSongName = Buffer.from(songUrl, 'ascii').toString('base64');
+	const songPath = './tmp/' + internalSongName + '.mp3';
 
-	const songData = await fetch(songUrl);
+	if (fs.existsSync(songPath)) {
+		// Serve song from internal cache
 
-	return songData;
+		const blob = fs.readFileSync(songPath);
+		return new Response(blob);
+	} else {
+		// Request song and save it.
+
+		const songData = await fetch(songUrl);
+		const blob = await songData.blob();
+		const buffer = Buffer.from(await blob.arrayBuffer());
+
+		if (!fs.existsSync('./tmp')) {
+			fs.mkdirSync('./tmp');
+		}
+
+		fs.writeFile(songPath, buffer, (err) => {
+			if (err) {
+				console.log('Could not save mp3', err);
+			}
+		});
+
+		return new Response(blob);
+	}
 }
