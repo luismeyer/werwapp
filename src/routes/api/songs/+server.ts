dfcf47438036574e682f2ac66b39a59b37a8a03b
Bjarne Rentz <Bjarnerentz@live.de>
2023-02-11 19:15:24 +0100

    feat: Added compression upon first request

--- src/routes/api/songs/+server.ts
+++ src/routes/api/songs/+server.ts
@@ -1,7 +1,7 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import fs from 'fs';
 import { createGzip } from 'zlib';
-import { pipeline, Readable } from 'stream';
+import { pipeline, Readable, Stream } from 'stream';
 import { promisify } from 'util';
 
 export async function GET({ url }: RequestEvent) {
@@ -9,32 +9,36 @@ export async function GET({ url }: RequestEvent) {
 	const internalSongName = Buffer.from(songUrl, 'ascii').toString('base64url');
 	const songPath = './tmp/' + internalSongName + '.gz';
 
+	let response: Response;
+
 	if (fs.existsSync(songPath)) {
 		// Serve song from internal cache
 		console.log('Serving mp3 from cache');
 		const blob = fs.readFileSync(songPath);
 
-		const response = new Response(blob);
-		response.headers.append('cache-control', 'public, max-age=31536000');
-		response.headers.append('content-encoding', 'gzip');
-		return response;
+		response = new Response(blob);
 	} else {
 		// Request song and save it.
 		const songData = await fetch(songUrl);
 		const blob = await await songData.blob();
+		const buffer = Buffer.from(await blob.arrayBuffer());
+		const bufferStream = Readable.from(buffer);
 
-		saveSong(blob, songPath);
+		saveSong(buffer, songPath);
 
-		const response = new Response(blob);
-		response.headers.append('cache-control', 'public, max-age=31536000');
-		return response;
+		const responseBuffer = await streamToBuffer(bufferStream.pipe(createGzip()));
+
+		response = new Response(responseBuffer);
 	}
+
+	response.headers.append('cache-control', 'public, max-age=31536000');
+	response.headers.append('content-encoding', 'gzip');
+	return response;
 }
 
-const saveSong = async (blob: Blob, songPath: string) => {
+const saveSong = async (buffer: Buffer, songPath: string) => {
 	const gzip = createGzip();
 	const pipe = promisify(pipeline);
-	const buffer = Buffer.from(await blob.arrayBuffer());
 	const bufferStream = Readable.from(buffer);
 
 	const destination = fs.createWriteStream(songPath);
@@ -47,5 +51,15 @@ const saveSong = async (blob: Blob, songPath: string) => {
 
 	await pipe(Readable.from(buffer), gzip, destination);
 	bufferStream.destroy();
+	gzip.close();
 	destination.close();
+};
+
+const streamToBuffer = (stream: Stream): Promise<Buffer> => {
+	const chunks: Buffer[] = [];
+	return new Promise((resolve, reject) => {
+		stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+		stream.on('error', (err) => reject(err));
+		stream.on('end', () => resolve(Buffer.concat(chunks)));
+	});
 };
