35ef814550b9e4dbe666f9f2a84ea1ec30979906
Luis Meyer <luis.meyer@vercel.com>
2025-01-07 22:33:54 +0100

    Refactor forest to img

--- src/components/forest.svelte
+++ src/components/forest.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
 </script>
 
-<div id="forest" class="forest"></div>
+<img src="/forest.svg" alt="Forest silhouette" class="forest" />
 
 <style>
 	.forest {
 		position: fixed;
-		background-image: url('/forest.svg');
 		/* Place on top edge of bottom nav */
-		bottom: 4rem;
+		bottom: 4.5rem;
 		height: 70px;
 		width: 100vw;
-		background-repeat: no-repeat;
-		background-position-x: 50%;
+		margin: 0 auto;
+		object-fit: cover;
 	}
 </style>
